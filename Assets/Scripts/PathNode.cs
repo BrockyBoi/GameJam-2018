85868d951eefb5d83a106e6d655d0106da2f881a
Brock Soicher <brocksoicher@msn.com>
2018-01-27 13:36:35 -0700

    Audio and Minor Fixes

--- Assets/Scripts/PathNode.cs
+++ Assets/Scripts/PathNode.cs
@@ -9,11 +9,25 @@ public class PathNode : MonoBehaviour
     static List<PathNode> nodes = new List<PathNode>();
     public static List<PathNode> Nodes { get { return nodes; } }
     public static PathNode RandomNode { get { return nodes[Random.Range(0, nodes.Count)]; } }
+    static Transform parentTransform;
+    static Transform ParentTransform
+    {
+        get
+        {
+			if(parentTransform == null)
+			{
+				parentTransform = new GameObject("Path Nodes").transform;
+			}
+			return parentTransform;
+        }
+		
+    }
     void Awake()
     {
         nodes.Add(this);
         GetComponent<MeshRenderer>().enabled = false;
-    }
 
+		transform.SetParent(ParentTransform);
+    }
 
 }

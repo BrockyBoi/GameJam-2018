af1c99ca6d8c9efbfbc094239c0433964ffa92da
Brock Soicher <brocksoicher@msn.com>
2018-01-28 11:35:04 -0700

    Rune Implementation

---
+++ Assets/Scripts/Rune/RuneSpin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuneSpin : MonoBehaviour
+{
+    [SerializeField]
+    [Range(20, 100)]
+    float rotateSpeed = 50;
+    public Transform rotatePoint;
+
+    void Update()
+    {
+        if (rotatePoint)
+            transform.RotateAround(rotatePoint.position, Vector3.up, rotateSpeed * Time.deltaTime);
+        else transform.Rotate(new Vector3(0, rotateSpeed * Time.deltaTime, 0));
+    }
+}
--- Assets/Scripts/Rune/RuneStation.cs
+++ Assets/Scripts/Rune/RuneStation.cs
@@ -7,11 +7,12 @@ public class RuneStation : MonoBehaviour {
 	public bool correctButtonPressed = false;
 	static int numStations = 0;
 	int stationIndex;
-	[SerializeField]
-	Material runeMat;
+
 	public enum Colors{Red, Blue, NeonGreen, Purple}
 
 	Colors correctColor;
+
+	public Transform runeSpot;
 
 	void Awake() {
 		stationIndex = numStations;
@@ -21,9 +22,13 @@ public class RuneStation : MonoBehaviour {
 	void Start()
 	{
 		correctColor = RuneManager.RuneKey[stationIndex];
-		// assign correct material by asking RuneManager which rune to use
-		runeMat = Resources.Load ("Materials/Runes/Rune"+stationIndex) as Material;
-		transform.Find("Rune").GetComponent<MeshRenderer>().material = runeMat;
+		GameObject rune = Instantiate(Resources.Load("Runes/Rune " + (stationIndex + 1)) as GameObject, Vector3.zero, Quaternion.identity);
+		Vector3 scale = rune.transform.localScale;
+		rune.transform.SetParent(transform);
+		rune.transform.localScale = scale;
+		rune.transform.localPosition = new Vector3(4.47f, -.611f, .204f);
+		rune.transform.parent = null;
+		rune.transform.localScale = scale;
 	}
 
 	public void ButtonPressed(Colors colorPressed, DelegateManager.EmptyVoid successCallback, DelegateManager.EmptyVoid failCallback)

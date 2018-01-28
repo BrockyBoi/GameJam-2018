422c66e153d2847607c42510f64b74905d3831a4
Derek Strobel <dnstrobel000@gmail.com>
2018-01-28 10:02:10 -0700

    added lights n shit

---
+++ Assets/Scripts/UI/ColorUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorUI : MonoBehaviour {
+
+	[SerializeField]
+	int index;
+	// Use this for initialization
+	void Start () {
+		GetComponent<Image>().material = Resources.Load("Materials/Colors/"+RuneManager.RuneKey[index].ToString()) as Material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
